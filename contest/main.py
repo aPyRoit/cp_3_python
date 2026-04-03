2ad517186429abf61f6cc773fdad8a0d7d194e51
aPyRoit <art_mal_YT@mail.ru>
2026-04-03 18:19:14 +0300

    4testsPassed

--- contest/main.py
+++ contest/main.py
@@ -1,16 +1,55 @@
 def array_diff(a: list, b: list) -> list:
-    """Решение для Задачи 1"""
-    pass
+    return [x for x in a if x not in b]
+
 
 def sum_pairs(nums: list, sum: int) -> list:
-    """Решение для Задачи 2"""
-    pass
+    seen = set()
+    for x in nums:
+        complement = sum - x
+        if complement in seen:
+            return [complement, x]
+        seen.add(x)
+    return None
+
 
 def remove_duplicate_ids(obj: dict) -> dict:
-    """Решение для Задачи 3"""
-    pass
+    sorted_keys = sorted(obj.keys(), key=lambda k: int(k))
 
-# В данной функции определите самостоятельно, что она принимает, а что возвращает
-def lazy():
-    """Решение для Задачи 4"""
-    pass
+    char_owner = {}
+    for key in sorted_keys:
+        for char in obj[key]:
+            char_owner[char] = key
+    result = {key: [] for key in obj}
+    for key in sorted_keys:
+        seen_in_array = set()
+        for char in obj[key]:
+            if char_owner[char] == key and char not in seen_in_array:
+                result[key].append(char)
+                seen_in_array.add(char)
+
+    return result
+
+
+def lazy(n: int):
+    def decorator(func):
+        call_count = [0]
+
+        def wrapper(*args, **kwargs):
+            call_count[0] += 1
+            count = call_count[0]
+
+            if n == -1:
+                return None
+            elif n == 1:
+                return func(*args, **kwargs)
+            elif n > 0:
+                if (count - 1) % n == 0:
+                    return func(*args, **kwargs)
+                return None
+            else:
+                if count % (-n) == 0:
+                    return None
+                return func(*args, **kwargs)
+
+        return wrapper
+    return decorator
